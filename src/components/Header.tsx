f0689fb8f294fc2c5f99f4c73f83c415765ec7a3
rodrigo <rodrigogaluppo788@gmail.com>
2023-06-12 15:03:02 +0100

    change pwa image

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -75,7 +75,7 @@ export default function Header() {
           <HStack spacing={8} alignItems={'center'}>
             <Box w="100%" >
                   <LinkRoute to="/" >
-                  <Image w="100%" maxH={"80px"} objectFit={"contain"} src={useColorModeValue('../img/logo-white.svg', '../img/logo-dark.svg')}/>
+                  <Image w="100%" maxH={"80px"} objectFit={"contain"} src={useColorModeValue('../Images/logo-white.svg', '../Images/logo-dark.svg')}/>
                   </LinkRoute>
             </Box>
             <HStack
